3f4a7d9abe15bc8b9bad3cfb0aca920c1857ee31
ljaviertovar <luisjavier_tovar@outlook.com>
2023-01-13 21:11:34 -0500

    add controllers

--- backend/src/routes/github-routes.ts
+++ backend/src/routes/github-routes.ts
@@ -1,18 +1,16 @@
 import express, { Request, Response, Router } from 'express';
-import * as githubServices from '../controllers/github-controller';
+import { getAccessToken, getUserData } from '../controllers/github-controller';
 
 const router: Router = express.Router();
 
 router.get('/accessToken', (req: Request, res: Response) => {
   const code = req.query.code;
-  githubServices.getAccessToken(code as string).then((resp) => res.json(resp));
+  getAccessToken(code as string).then((resp) => res.json(resp));
 });
 
 router.get('/userData', (req: Request, res: Response) => {
   const accessToken = req.query.accessToken;
-  githubServices
-    .getUserData(accessToken as string)
-    .then((resp) => res.json(resp));
+  getUserData(accessToken as string).then((resp) => res.json(resp));
 });
 
 export default router;
--- backend/src/routes/google-routes.ts
+++ backend/src/routes/google-routes.ts
@@ -1,13 +1,11 @@
 import express, { Request, Response, Router } from 'express';
-import * as googleServices from '../controllers/google-controller';
+import { getUserData } from '../controllers/google-controller';
 
 const router: Router = express.Router();
 
 router.get('/userData', (req: Request, res: Response) => {
   const accessToken = req.query.accessToken;
-  googleServices
-    .getUserData(accessToken as string)
-    .then((resp) => res.json(resp));
+  getUserData(accessToken as string).then((resp) => res.json(resp));
 });
 
 export default router;
